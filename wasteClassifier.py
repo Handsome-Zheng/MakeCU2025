82431870a5338b8e066159ab70db5e02b7b41730
Handsome Zheng <handsomezheng123@gmail.com>
2025-11-08 10:09:27 -0500

    Changed Data_DIR to "data"

--- wasteClassifier.py
+++ wasteClassifier.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 # ==================== CONFIGURATION ====================
 class Config:
     # Paths (modify these to point to your dataset)
-    DATA_DIR = '/Users/minhthanhnguyen/Desktop/robotics/data/wasteData/images/images'
+    DATA_DIR = 'data'
     
     # Data split ratios
     TRAIN_RATIO = 0.7
